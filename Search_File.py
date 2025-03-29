110835cdf8bc491f86b641bbfed890e7c6597cc9
Tanishq Chauhan <tanishqchauhan999@gmail.com>
2025-03-29 18:56:42 +0530

    Update Search_File.py

--- Search_File.py
+++ Search_File.py
@@ -1,29 +1,26 @@
-
-
 # Import the libraries
 import os
 import pandas as pd
-import numpy as np
 
 # Assign the variables
-sourceLoc="D:/Learnerea/others/"
-outLoc="D:/Learnerea/temp/"
-searchString="LEARNEREA"
-fileType=".txt"
+sourceLoc = "D:/Learnerea/others/"
+outLoc = "D:/Learnerea/temp/"
+searchString = "LEARNEREA"
 
 direc = os.listdir(sourceLoc)
-direc
-
 fileList = []
-# For loop to search for the string
+
+# For loop to search for the string in all files
 for file in direc:
-    if file.endswith(fileType):
-        f=open(sourceLoc+file,'r')
-        if searchString in f.read():
-            fileList.append(file)
-        f.close()
-        
+    try:
+        # Try to open and read the file as text
+        with open(sourceLoc + file, 'r', encoding='utf-8') as f:
+            if searchString in f.read():
+                fileList.append(file)
+    except:
+        # If there's an error (like binary files), skip the file
+        continue
 
 # DataFrame creation and export to excel
-stringFile = pd.DataFrame(fileList,columns=['FileName'],index=range(0,len(fileList)))
-stringFile.to_excel(outLoc+"stringFile.xlsx",index=False)
+stringFile = pd.DataFrame(fileList, columns=['FileName'], index=range(0, len(fileList)))
+stringFile.to_excel(outLoc + "stringFile.xlsx", index=False)                  
